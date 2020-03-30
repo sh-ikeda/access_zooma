84e4d9298287bd84a0ee43f1f4e4b2e4a0ef5735
sh-ikeda <togotv_dell1@DESKTOP-96BPKRE.localdomain>
2020-03-30 14:46:07 +0900

    added option to specify attributes to collect with less priority

--- cmd/gen_zooma_query/main.go
+++ cmd/gen_zooma_query/main.go
@@ -33,12 +33,14 @@ func parseJSON(bytes []byte, d *interface{}) {
 
 func main() {
 	a := flag.String("a", "", "comma-separated string of attributes to be collected")
+	b := flag.String("b", "", "comma-separated string of attributes to be collected with less priority")
 	s := flag.Bool("s", false, "If true, a value of attributes not listed in -a option is separated with whitespace and each word is output as a query")
 	l := flag.Bool("l", false, "If true, all attributes are output for entries without any attributes listed in -a")
 	t := flag.Int("t", 9606, "Taxonomy ID of species of interest. Default is 9606; human.")
 	flag.Parse()
 
 	attr := strings.Split(*a, ",")
+	attr_less_prior := strings.Split(*b, ",")
 	outputAll := *l
 	toBeSeparated := *s
 	taxIdOfInterest := *t
@@ -58,13 +60,14 @@ func main() {
 	fmt.Fprintln(os.Stderr, "Writing...")
 	for _, data := range decode_data.([]interface{}) {
 		d := data.(map[string]interface{})
-		id := d["accession"].(string)
+		sampleId := d["accession"].(string)
 		taxId := d["taxId"].(float64)
 		if int(taxId) != taxIdOfInterest {
 			continue
 		}
 		ch := d["characteristics"].(map[string]interface{})
-		n := 0
+		has_key := false
+		outstr_less_prior := ""
 		for key, _ := range ch {
 			if contains(attr, key) || *a == "" {
 				c := ch[key].([]interface{})
@@ -72,27 +75,39 @@ func main() {
 				value := x["text"].(string)
 				// in case that value has "\t"
 				value = strings.Replace(value, "\t", " ", -1)
-				fmt.Fprintf(os.Stdout, "%s\t%s\t%s\t\n", id, key, value)
-				n++
+				fmt.Fprintf(os.Stdout, "%s\t%s\t%s\t\n", sampleId, key, value)
+				has_key = true
+			}
+			if contains(attr_less_prior, key) {
+				c := ch[key].([]interface{})
+				x := c[0].(map[string]interface{})
+				value := x["text"].(string)
+				// in case that value has "\t"
+				value = strings.Replace(value, "\t", " ", -1)
+				outstr_less_prior += fmt.Sprintf("%s\t%s\t%s\t\n", sampleId, key, value)
 			}
 		}
-		// When no attribute contains any keywords, output all
-		if n == 0 && outputAll {
+		// When a sample has no specified key, output all
+		if !has_key && outputAll {
 			for key, _ := range ch {
 				c := ch[key].([]interface{})
 				x := c[0].(map[string]interface{})
 				value := x["text"].(string)
 				//slice := strings.Split(value, " ")
 				slice := regexp.MustCompile("[ (),./]+").Split(value, -1)
-				fmt.Fprintf(os.Stdout, "%s\t%s\t%s\t\n", id, key, value)
+				fmt.Fprintf(os.Stdout, "%s\t%s\t%s\t\n", sampleId, key, value)
 				if toBeSeparated && len(slice) > 1 {
 					for _, word := range slice {
 						if word != "" {
-							fmt.Fprintf(os.Stdout, "%s\t%s\t%s\t%s\n", id, key, word, value)
+							fmt.Fprintf(os.Stdout, "%s\t%s\t%s\t%s\n", sampleId, key, word, value)
 						}
 					}
 				}
 			}
+		}
+
+		if !has_key && outstr_less_prior != "" {
+			fmt.Fprintf(os.Stdout, "%s", outstr_less_prior)
 		}
 	}
 	fmt.Fprintln(os.Stderr, "Done.")
